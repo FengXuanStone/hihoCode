64e2417564f3fc2c2c7e7510babcbcb2b45f603c
fengxuan <651353997@qq.com>
2016-07-20 10:20:26 +0800

    update character Deletion and repair Clock

--- src/level1/LostInTheCity/Main.java
+++ src/level1/LostInTheCity/Main.java
@@ -17,43 +17,63 @@ public class Main {
         }
     }
 
-    public static ArrayList<myPoint> find(char[][] maps, char[][] neighbor)  {
-        char[] mid = neighbor[1];
-        ArrayList<myPoint> pointArr = new ArrayList<myPoint>();
+    public static char[][] xuanzhuan(char a[][],int N){
+        char[][] b = new char[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                b[N-1-j][N-1-i] = a[i][N-1-j];
+            }
+        }
+        return b;
+    }
 
-        for(int j = 0; j < maps[0].length - 2; j++) {
-            for(int i = 1; i < maps.length - 1; i++) {
-                if(maps[i][j] == mid[0] &&
-                        maps[i][j + 1] == mid[1] &&
-                        maps[i][j + 2] == mid[2]) {
-                    if(maps[i - 1][j] == neighbor[0][0] &&
-                            maps[i - 1][j + 1] == neighbor[0][1] &&
-                            maps[i - 1][j + 2] == neighbor[0][2]) {
-                        if(maps[i + 1][j] == neighbor[2][0] &&
-                                maps[i + 1][j + 1] == neighbor[2][1] &&
-                                maps[i + 1][j + 2] == neighbor[2][2]) {
-                            myPoint tmp = new myPoint(i + 1, j + 2);
-                            pointArr.add(tmp);
-                        }
-                    }
-                } else if(maps[i][j] == mid[2] &&
-                        maps[i][j + 1] == mid[1] &&
-                        maps[i][j + 2] == mid[0]) {
-                    if(maps[i - 1][j] == neighbor[2][2] &&
-                            maps[i - 1][j + 1] == neighbor[2][1] &&
-                            maps[i - 1][j + 2] == neighbor[2][0]) {
-                        if(maps[i + 1][j] == neighbor[0][2] &&
-                                maps[i + 1][j + 1] == neighbor[0][1] &&
-                                maps[i + 1][j + 2] == neighbor[0][0]) {
-                            myPoint tmp = new myPoint(i + 1, j + 2);
-                            pointArr.add(tmp);
-                        }
-                    }
+    public static void rotate(char a[][],int N) {
+        for (int layer = 0; layer < N / 2; layer++) {
+            int first = layer;
+            int last = N - 1 - layer;
+            for (int i = layer; i < last; i++) {
+                int offset = i - layer;
+                char top = a[first][i];
+                a[first][i] = a[last - offset][first];
+                a[last - offset][first] = a[last][last - offset];
+                a[last][last - offset] = a[i][last];
+                a[i][last] = top;
+            }
+        }
+    }
+
+    public static boolean match(char[][] maps, char[][] neighbor, int i, int j) {
+        for(int x = 0; x < neighbor.length; x++) {
+            for(int y = 0; y < neighbor[0].length; y++) {
+                if(maps[i + x][j + y] != neighbor[x][y]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static ArrayList<myPoint> find(char[][] maps, char[][] neighbor)  {
+        ArrayList<myPoint> result = new ArrayList<>();
+
+        for(int i = 0; i < maps.length - neighbor.length + 1; i++) {
+            for(int j = 0; j < maps[0].length - neighbor[0].length + 1; j++) {
+                boolean b1 = match(maps, neighbor, i , j);
+                rotate(neighbor, neighbor.length);
+                boolean b2 = match(maps, neighbor, i , j);
+                rotate(neighbor, neighbor.length);
+                boolean b3 = match(maps, neighbor, i , j);
+                rotate(neighbor, neighbor.length);
+                boolean b4 = match(maps, neighbor, i , j);
+                rotate(neighbor, neighbor.length);
+                if(b1 || b2 || b3 || b4) {
+                    result.add(new myPoint(i + neighbor.length / 2 + 1,
+                            j + neighbor.length / 2 + 1));
                 }
             }
         }
 
-        return pointArr;
+        return result;
     }
 
     public static void main(String[] args) throws Exception {
