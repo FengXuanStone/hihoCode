349418e485dff419061a1d7a62b39a3a00ddf31c
fengxuan <651353997@qq.com>
2016-07-20 21:15:09 +0800

    update magic box and recruitment...

---
+++ src/level1/Inequality/Main.java
@@ -0,0 +1,17 @@
+package level1.Inequality;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by fengxuan on 2016/7/20.
+ */
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+
+    }
+}
